7b41752fc241767a53c14151764b2333350d8d71
unknown <nelsonaldevaran@gmail.com>
2021-10-20 09:50:32 -0500

    inicio de proyecto

---
+++ class/conexion.php
@@ -0,0 +1,21 @@
+<?php 
+require("parametros.php");
+class Conexion
+{
+
+	protected $conex;
+
+	public function Conexion()
+	{
+		try
+		{
+			$this->conex= new PDO("mysql:host=".serv.";dbname=".base.";charset=".charset.";port=".port,usua,pass);
+		}
+		catch(exception $e)
+		{
+			echo"Error al aconectar la base de datos".$e->getMessage();
+		}
+
+	}
+}
+?>
---
+++ class/parametros.php
@@ -0,0 +1,8 @@
+<?php 
+	define("serv","localhost");
+	define("base","colegio");
+	define("usua","root");
+	define("pass","");
+	define("charset","utf8");
+	define("port","3306");
+?>
