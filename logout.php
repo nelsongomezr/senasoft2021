226aee414c86c5012bb89b969ad58c064bf2d190
unknown <APRENDIZ@SOAAPRCIDFSD006.FormacionSena.RED>
2021-10-22 09:05:13 -0500

    final

---
+++ logout.php
@@ -0,0 +1,8 @@
+<?php
+@session_start();
+if (session_destroy());
+echo "<script type='text/javascript'>
+        alert('Haz cerrado Sesión exitosamente...!!Hasta Pronto¡¡');
+        window.location='index.php';
+        </script>";
+?>
